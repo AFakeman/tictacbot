86a5c95ca77f34fe91592aa8ffd20a61e8cfa1bb
AFakeman <anton_suslov@me.com>
2017-03-06 20:56:51 +0300

    Fixed #4

--- tictacbot/views.py
+++ tictacbot/views.py
@@ -31,21 +31,26 @@ def arguments(*arg_types):
 def get_client(client):
     if client not in info:
         info[client] = {
-            "another_one": "",
-            "board": "",
-            "bot": "on"
+            "another_one": "",  # whether the user was offered another game
+            "board": "",  # current state of the board in str representation
+            "bot": "on",  # turn the AI on or off
+            "x": "",  # x's player, "#bot' is for the AI player
+            "o": "",  # o's player
         }
-    return info[client]
+    info_obj = info[client]
+    info_obj.decode = "UTF-8"
+    return info_obj
 
 @bot.command_handler(
     command='start',
-    doc="/start <size> (<side>) - start with a <size> cell field (between 3 and 50), <side> is x or o. X's go first!",
+    doc="/start <size> (<side>) - start with a <size> cell field "
+        "(between 3 and 50), <side> is x or o. X's go first!",
     pass_args=True
 )
 @arguments((int, str), (int,), ())
-def start_game(update, args):
+def start_game(update, args, restart=False):
     if not args:
-        print("New client: {0}!".format(str(update.message.from_user.username)))
+        print("New client: {0}!".format(update.message.from_user.username))
         return "Hello! Type start <size> to play the game, or use /help!"
     size = args[0]
     if len(args) == 2:
@@ -53,17 +58,33 @@ def start_game(update, args):
     else:
         start = choice(["x", "o"])
     client = get_client(update.message.chat_id)
-    if client["board"]:
+    if client["board"] and not restart:
         return "You still have a game playing."
     if client["another_one"]:
         client["another_one"] = ""
     if start not in ['o', 'x']:
         raise ValueError("Invalid starting piece")
     board = TicTacToe(field_size=size)
-    if start == 'o' and client["bot"].decode('ascii') == 'on':
+    opposite = board.opposite(start)
+    if not restart:
+        if client["bot"] == 'on':
+            client[start] = '#any'
+            client[opposite] = "#bot"
+        else:
+            client[start] = update.message.from_user.username
+            client[opposite] = "#undefined"
+    response = []
+    if client["x"] == "#bot":
         TicTacPlayer.move(board)
+        response.append(("img", board.img()))
+    elif client["x"] == "#undefined":
+        response.append(("img", board.img()))
+        response.append("The first turn is the other player's! /join to play!")
+
+    else:
+        response.append(("img", board.img()))
     client["board"] = repr(board)
-    return [("img", board.img())]
+    return response
 
 
 
@@ -77,13 +98,19 @@ def process_move(update, args):
     x = args[0]
     y = args[1]
     client = get_client(update.message.chat_id)
+    response = []
     if not client["board"]:
         raise GameError("You are not playing. Care to /start another one?")
-    board = TicTacToe(client["board"].decode('ascii'))
+    board = TicTacToe(client["board"])
     if board.end:
         raise GameError("Game is over, let it go. Or /start another one.")
+    if client[board.turn] != update.message.from_user.username and client[board.turn] != "#any":
+        if client[board.opposite(board.turn)] == update.message.from_user.username:
+            return "It's not your turn!"
+        else:
+            return "Only the player who /start and who /join may play"
     board.move(x - 1, y - 1)
-    if not board.end and client["bot"].decode('ascii') == "on":
+    if client[board.turn] == "#bot":
         TicTacPlayer.move(board)
     client["board"] = repr(board)
     img = board.img()
@@ -98,6 +125,23 @@ def process_move(update, args):
         client["another_one"] = "yes"
     return reply
 
+@bot.command_handler(
+    command='join',
+    doc='/join - join the game created by another player. '
+        'Works only if the bot is off.',
+)
+def process_join(update):
+    client = get_client(update.message.chat_id)
+    if client["bot"] == "on":
+        return "You have to turn the bot off to join the game."
+    if client["x"] == "#undefined":
+        client["x"] = update.message.from_user.username
+        return "You play as x's!"
+    elif client["o"] == "#undefined":
+        client["o"] = update.message.from_user.username
+        return "You play as o's!"
+    else:
+        return "Both players seem to be present."
 
 @bot.command_handler(
     command='end_game',
@@ -108,6 +152,8 @@ def end_game(update, args):
     client = get_client(update.message.chat_id)
     if client["board"]:
         client["board"] = ""
+        client["x"] = ""
+        client["y"] = ""
         return "You must be busy. Maybe other time."
     else:
         return "You were not playing in the first place..."
@@ -121,7 +167,7 @@ def agree(update):
         board = client["board"]
         size = int(len(board) ** 0.5)
         client["board"] = ""
-        return start_game(update, (size, choice(['x','o'])))
+        return start_game(update, (size, choice(['x','o'])), restart=True)
     else:
         return "Huh?"
 
@@ -132,6 +178,8 @@ def disagree(update):
     if client["another_one"]:
         client["another_one"] = ""
         client["board"] = ""
+        client["x"] = ""
+        client["y"] = ""
         return "You must be busy. Maybe other time."
     else:
         return "Huh?"
@@ -140,19 +188,21 @@ def disagree(update):
 @bot.command_handler(
     command='bot',
     pass_args=True,
-    doc="/bot <on/off> - turn bot on and off. You cannot start the game as o's when the bot is off!"
+    doc="/bot <on/off> - turn bot on and off."
 )
 @arguments((str,), ())
 def toggle_bot(update, args):
     client = get_client(update.message.chat_id)
     if not args:
-        return "Bot is {0} now.".format(client["bot"].decode('ascii'))
+        return "Bot is {0} now.".format(client["bot"])
     mode = args[0]
     if mode not in ["on", "off"]:
         raise ParseError("Bot can be either on or off.")
-    if mode == client["bot"].decode('ascii'):
+    if mode == client["bot"]:
         return "Bot is already {0}.".format(mode)
     else:
+        if client["board"] != "":
+            return "You can't toggle the bot during the game!"
         client["bot"] = mode
         return "Bot is {0} now.".format(mode)
 
